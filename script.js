9347e0eff786ecbff31f4258671af2e3fb8ff61e
ZacZe <zachariah.zergoug@gmail.com>
2024-12-27 01:23:20 +0000

    update contacts.html, script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function themeToggler() { // Toggles light or dark mode accordingly. 
     const themeToggle = document.getElementById("theme-toggle");
-    const body = Document.body; 
+    const body = document.body; 
 
     themeToggle.addEventListener("click", () => {
-        if ( body.classList.toggle("dark-mode") ) { // If dark mode, 
+        if ( body.classList.contains("dark-mode") ) { // If dark mode, 
             body.classList.remove("dark-mode");     // remove dark mode, 
             body.classList.add("light-mode")        // add light mode. 
         } else {                                    // Else if light mode, 
@@ -13,8 +13,8 @@ function themeToggler() { // Toggles light or dark mode accordingly.
     }); 
 }
 
-function copyToClipboard(specified_ID) { // Copies value with specific ID to user"s clipboard
-    var text = document.getElementById(specified_ID);
+function copyToClipboard() { // Copies value with specific ID to user"s clipboard
+    var text = document.getElementById("copyText");
 
     text.select();                    // for computers
     text.setSelectionRange(0, 99999); // for phones

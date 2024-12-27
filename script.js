3d9c7d62ec8c54d2cae7c45eb1c409e5bd3be719
ZacZe <zachariah.zergoug@gmail.com>
2024-12-27 00:57:19 +0000

    update contacts, index, script

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-const themeToggle = document.getElementById('theme-toggle');
-const body = Document.body; 
+function themeToggler() { // Toggles light or dark mode accordingly. 
+    const themeToggle = document.getElementById('theme-toggle');
+    const body = Document.body; 
 
-toggleButton.addEventListener('click', () => {
-    if ( body.classList.toggle('dark-mode') ) { // if dark mode
-        body.classList.toggle('light-mode')
-    } else {
-        body.classList.toggle('dark-mode')
-    }
-}); 
+    toggleButton.addEventListener('click', () => {
+        if ( body.classList.toggle('dark-mode') ) { // If dark mode, 
+            body.classList.toggle('light-mode')     // turn to light mode.
+        } else {                                    // Else if light mode, 
+            body.classList.toggle('dark-mode')      // turn to dark mode.
+        }
+    }); 
+}
+
+function copyToClipboard(specified_ID) { // Copies value with specific ID to user's clipboard
+    var text = document.getElementById(specified_ID);
+
+    text.select();                    // for computers
+    text.setSelectionRange(0, 99999); // for phones
+
+    navigator.clipboard.writeText(text.value);
+}

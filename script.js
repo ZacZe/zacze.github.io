02172f93ee8ca3840e1d8dd04402d78edfc21e98
ZacZe <zachariah.zergoug@gmail.com>
2024-12-29 22:52:50 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function changeTheme() {
+function changeThemeSaved() {
     const toggleButton = document.getElementById("theme-toggle");
     const LIGHT_MODE = "light";
     const DARK_MODE = "dark";
@@ -8,16 +8,34 @@ function changeTheme() {
     if ( savedTheme == DARK_MODE ) {
         localStorage.setItem("theme", LIGHT_MODE);
         toggleButton.innerHTML = "Dark Mode"; 
-    } else {
+    } else if ( savedTheme == LIGHT_MODE ) {
         localStorage.setItem("theme", DARK_MODE);
         toggleButton.innerHTML = "Light Mode"; 
     }
 }
 
+function setThemeSaved() {
+    const body = document.body; 
+    const LIGHT_MODE = "light";
+    const DARK_MODE = "dark";
+
+    body.classList.remove("light-mode");
+    body.classList.add("light-mode");
+
+    let savedTheme = localStorage.getItem("theme");
+
+    if ( savedTheme == DARK_MODE ) {
+        body.classList.toggle("light-mode");
+        toggleButton.innerHTML = "Dark Mode"; 
+    } else if ( savedTheme == LIGHT_MODE ) {
+        toggleButton.innerHTML = "Light Mode"; 
+    } 
+}
+
 function themeToggler() { // Toggles light or dark mode accordingly. 
     const body = document.body; 
 
-    body.classList.toggle("light-mode"); // Turns on/off depending if off/on.
+    body.classList.toggle("light-mode"); // Turns on/off if previously off/on. 
 
     changeTheme();
 }

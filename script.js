5ddafb1b873a23b307ab7b31132d7a425ef46e92
ZacZe <zachariah.zergoug@gmail.com>
2024-12-29 23:49:01 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-function themeToggler() {
+function themeToggler() { // toggles the theme 
     const toggleButton = document.getElementById("theme-toggle");
     const body = document.body; 
     const LIGHT_MODE = "light";
     const DARK_MODE = "dark";
 
-    let savedTheme = localStorage.getItem("theme");
+    let savedTheme = localStorage.getItem("theme")
 
-    if ( savedTheme == DARK_MODE ) {
-        body.classList.toggle("light-mode", false);
+    if ( savedTheme == DARK_MODE ) { // if saved as dark mode 
+        body.classList.add("light-mode");
         localStorage.setItem("theme", LIGHT_MODE);
         toggleButton.innerHTML = "Dark Mode"; 
-    } else if ( savedTheme == LIGHT_MODE ) {
-        body.classList.toggle("light-mode", true);
+    } else { // if saved as light mode
+        body.classList.remove("light-mode");
         localStorage.setItem("theme", DARK_MODE);
         toggleButton.innerHTML = "Light Mode"; 
-    }
+    } 
 }
 
-function setThemeOnLoad() {
+function setThemeOnLoad() { // sets theme depending on the saved theme
     const toggleButton = document.getElementById("theme-toggle");
     const body = document.body; 
     const LIGHT_MODE = "light";
@@ -28,10 +28,10 @@ function setThemeOnLoad() {
     let savedTheme = localStorage.getItem("theme");
 
     if ( savedTheme == DARK_MODE ) {
-        body.classList.toggle("light-mode", false);
+        body.classList.add("light-mode");
         toggleButton.innerHTML = "Dark Mode"; 
-    } else if ( savedTheme == LIGHT_MODE ) {
-        body.classList.toggle("light-mode", true);
+    } else {
+        body.classList.remove("light-mode");
         toggleButton.innerHTML = "Light Mode"; 
     } 
 
